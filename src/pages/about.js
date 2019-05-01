3897d5509d7ad461767a469124cb27c4a120ed42
cineron <cineron+github@gmail.com>
2019-04-30 22:52:00 -0500

    Add Layout compoent and uses it

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Link } from "gatsby";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Layout from "../components/layout";
 
 const AboutPage = () => {
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>About</h1>
             <h3>Ron Wilson</h3>
             <p>I'm a film and video editor turned software engineer focused on creating modern apps that connect people.</p>
-            {/* <p><Link to="/contact">Contact me</Link></p> */}
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,13 @@
 import React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Layout from "../components/layout";
 
 const ContactPage = () => {
     return (
-        <div>
-            <Header />
+        <Layout>
             <h1>Contact</h1>
             <h3>ron@cineron.com</h3>
             <p><a href="https://www.twitter.com/cineron" target="_blank" rel="noopener noreferrer">Tweet it up, yo.</a></p>
-            <Footer />
-        </div>
+        </Layout>
     )
 }
 
