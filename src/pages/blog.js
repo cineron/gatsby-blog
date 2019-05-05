0b94267ced6eb899f1fe8c350c3d4c48687eee00
cineron <cineron+github@gmail.com>
2019-05-05 15:30:59 -0500

    Adds blog template and dynamically renders page slug

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
-import { useStaticQuery, graphql } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 
 // CHALLENGE: Render list of Blog Titles and Dates 
 // 1. Query the title and date for each post in the blog component 
 // 2. Render an ol on the page 
 // 3. Render an li with a nested h2 (title) and p (date) for each post 
 //     - Google "render array of objects react" if needed
+
+// CHALLENGE 2: Link to Blog Posts 
+// 1. fetch the slug for the post
+// 2. use slug to generate link to the post page
 
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
@@ -17,6 +21,9 @@ const BlogPage = () => {
                         frontmatter{
                             title
                             date
+                        }
+                        fields {
+                                slug
                         }
                     }
                 }
@@ -33,8 +40,10 @@ const BlogPage = () => {
                 {data.allMarkdownRemark.edges.map((edge) => {
                     return (
                         <li>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                            <p>{edge.node.frontmatter.date}</p>
+                            <Link to={`/blog/${edge.node.fields.slug}`}>
+                                <h2>{edge.node.frontmatter.title}</h2>
+                            </Link>
+                                <p>{edge.node.frontmatter.date}</p>
                         </li>
                     )
                 })}
