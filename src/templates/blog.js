f2fafc4af85ed6282c1b485d8fcfdc42f5640369
cineron <cineron+github@gmail.com>
2019-05-06 12:57:25 -0500

    Adds the blog content from Contentful, including images.

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,33 +1,68 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
+// Query for Markdown
+// export const query = graphql`
+//     query (
+//         $slug: String!
+//     ){
+//         markdownRemark (
+//             fields: {
+//                 slug: {
+//                     eq: $slug
+//                 }
+//             }
+//         ){
+//             frontmatter {
+//             title
+//             date
+//             }
+//             html
+//         }
+//     }
+// `;
+
+// Query for Contentful
 export const query = graphql`
-    query (
-        $slug: String!
-    ){
-        markdownRemark (
-            fields: {
-                slug: {
-                    eq: $slug
-                }
+    query($slug: String!) {
+        contentfulBlogPost(slug: {eq: $slug}) {
+            title 
+            publishedDate(formatString: "MMMM Do, YYYY")
+            body {
+                json
             }
-        ){
-            frontmatter {
-            title
-            date
-            }
-            html
         }
     }
-`;
+`
 
+
+// for Markdown
+// const Blog = (props) => {
+//     return(
+//         <Layout>
+//             <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+//             <p>{props.data.markdownRemark.frontmatter.date}</p>
+//             <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+//         </Layout>
+//     );
+// };
 const Blog = (props) => {
+    const options = {
+        renderNode: {
+            "embedded-asset-block": (node) => {
+                const alt = node.data.target.fields.title["en-US"];
+                const url = node.data.target.fields.file["en-US"].url;
+                return <img alt={alt} src={url} />
+            }
+        }
+    }
     return(
         <Layout>
-            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-            <p>{props.data.markdownRemark.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html}}></div>
+            <h1>{props.data.contentfulBlogPost.title}</h1>
+            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
         </Layout>
     );
 };
