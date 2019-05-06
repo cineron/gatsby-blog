5f69c79474fedb36214d25247c29efceb1b7af49
cineron <cineron+github@gmail.com>
2019-05-06 13:28:41 -0500

    Adds Head file to dynamically update page title.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
+import Head from "../components/head";
 
 const ContactPage = () => {
     return (
         <Layout>
+            <Head title="Contact" />
             <h1>Contact</h1>
             <h3>ron@cineron.com</h3>
             <p><a href="https://www.twitter.com/cineron" target="_blank" rel="noopener noreferrer">Tweet it up, yo.</a></p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
+import Head from "../components/head";
 
 const IndexPage = () => {
     return (
         <Layout>
+            <Head title="Home" />
             <h1>Hello, World!</h1>
             <p>I'm Ron. A Front-End Engineer focusing on React!</p>
             {/* <p>Need a developer? <a href="/contact">Traditional link.</a></p> */}
